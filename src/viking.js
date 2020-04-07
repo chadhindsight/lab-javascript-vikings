d10c0d168c77b30c4451441d79fc49ad91f2d91b
Chad Hinds <chadrickj8@gmail.com>
2020-04-07 12:28:48 -0400

    First commit

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,53 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+
+        this.attack = function attack(){
+            return this.strength
+        };
+        this.receiveDamage = function receiveDamage(damage) {
+            this.health -= this.damage
+        }
+    } 
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name) {
+        super(health, strength)
+        this.receiveDamage = function receiveDamage(damage) {
+            this.health = this.damage
+            return this.health === 0 ? `${this.name}has died in act of combat` : `${this.name} has received ${this.damage}`
+        };
+        this.battleCry() = function battleCry() {
+            return "Odin Owns You All!"
+        }
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+    // constructor() {
+    //     super()
+    // }
+    receiveDamage(damage) {
+        this.health = this.damage
+        return this.health === 0 ? `A ${this.name}has died in act of combat` : `A ${this.name} has received ${this.damage}`
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor() {
+        this.vikingArmy = [];
+        this.saxonArmy =[];
+    }
+    // ARMY FUNCTIONS
+     addViking = function addViking() {
+
+    };
+    addViking = function addSaxon() {
+    }
+}
