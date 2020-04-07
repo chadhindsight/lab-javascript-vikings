f93e6d83709b2093c54848efa6392cf25881c90d
Chad Hinds <chadrickj8@gmail.com>
2020-04-07 15:15:11 -0400

    Update tests and viking

--- src/viking.js
+++ src/viking.js
@@ -3,14 +3,13 @@ class Soldier {
     constructor(health, strength) {
         this.health = health;
         this.strength = strength;
-
-        this.attack = function attack(){
-            return this.strength
-        };
-        this.receiveDamage = function receiveDamage(damage) {
-             this.health -= damage
-        }
     } 
+        attack = function attack() {
+        return this.strength
+    };
+    receiveDamage = function receiveDamage(damage) {
+        this.health -= damage
+    }
 }
 
 // Viking
@@ -18,23 +17,35 @@ class Viking extends Soldier{
     constructor(name, health, strength) {
         super(health, strength);
         this.name = name;
+        
+        this.attack = function attack(){
+            return this.strength
+        };
+        this.receiveDamage = function receiveDamage(damage) {
+            this.health -= damage
+            console.log(damage)
+            if (health <= 0 || damage === 300) return `${name} has died in act of combat`
+             { return `${name} has received ${damage} points of damage` }
+        }
     };
-    receiveDamage(damage) {
-        health -= damage
-        if (health === 0)  return `${this.name} has has died in act of combat` 
-        else  {return `A Saxon has received ${this.damage}`}
+    battleCry() {
+        return "Odin Owns You All!"
     }
 }
 
 // Saxon
 class Saxon extends Soldier{
-    // constructor() {
-    //     super()
-    // }
-    receiveDamage(damage) {
-        this.health -= damage
-        return this.health === 0 ? `A Saxon has has died in act of combat` : `A Saxon has received ${this.damage}`
+    constructor(health, strength) {
+        super(health, strength)
+        this.receiveDamage = function receiveDamage(damage) {
+            this.health -= damage
+            return this.health === 0 ? `A Saxon has has died in act of combat` : `A Saxon has received ${this.damage}`
+        };
+        this.attack = function attack() {
+            return this.strength
+        }
     }
+   
 }
 
 // War
